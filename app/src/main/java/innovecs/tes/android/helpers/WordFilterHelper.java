e33fd315900e8dffbfea0bc0fabc9c3e97eb3fcd
DenDen Den <DenDen Den>
2017-03-20 19:16:53 +0200

    Added comnments to rules

--- app/src/main/java/innovecs/tes/android/helpers/WordFilterHelper.java
+++ app/src/main/java/innovecs/tes/android/helpers/WordFilterHelper.java
@@ -8,22 +8,27 @@ import java.util.regex.Pattern;
 
 public class WordFilterHelper {
 
+    // Hello - h e l l o
     public static String addSpacesSenitise(String word) {
         return addCharacterToEach(word, "$0 ");
     }
 
+    // Hello - h.e.l.l.o
     public static String addDotsSenitise(String word) {
         return addCharacterToEach(word, "$0.");
     }
 
+    // Hello - h_e_l_l_o
     public static String addUnderlineSenitise(String word) {
         return addCharacterToEach(word, "$0_");
     }
 
+    // Hello - |-|ello
     public static String replaceSymbolHSenitise(String word) {
         return replaceSymbol(word, "H", "|-|");
     }
 
+    // Hello - h3llo
     public static String replaceSymbolESenitise(String word) {
         return replaceSymbol(word, "e", "3");
     }
